4800f2991e209cbc9229e2ecf58deb4b761270fc
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-01 14:21:32 -0600

    created functions that add a number to the input display when a number button is clicked, and add an operator to input display when operator is clicked

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,21 +6,30 @@ function onReady() {
     console.log('in onReady!');
     $('.calc-button[data-number]').on('click', addNumber);
     $('.calc-button[data-operator]').on('click', addOperator);
+    $('.calc-button[data-complete').on('click', equalObject);
     $('#clear-button').on('click', clearDisplay);
 }
 
 //add number to display
 function addNumber() {
     console.log('you clicked a number');
+    let number = $(this).data('number');
+    console.log(number);
+    $('#calc-display').val($('#calc-display').val() + number);
 };
 
 //add operator to display
 function addOperator() {
     console.log('you clicked an operator');
+    let operator = $(this).data('operator');
+    $('#calc-display').val($('#calc-display').val() + operator);
 };
 
 //create object from equation answer
-function equalObject() {};
+function equalObject() {
+    console.log('you clicked on the equals');
+    
+};
 
 //clear display
 function clearDisplay() {
