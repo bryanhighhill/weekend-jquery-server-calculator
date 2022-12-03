857246c76c46a40fe9c29cd818831731d57d4ca6
Bryan Highhill <bryanhighhill@gmail.com>
2022-12-03 13:11:48 -0600

    created equationHandler object that contains a function to split equations and call mathDoer function

--- server/public/scripts/equations.js
+++ server/public/scripts/equations.js
@@ -1,7 +1,28 @@
 let equationHandler = {
-    processAndSave: function processAndSave () {
-    
+    processAndSave: function processAndSave (equation) {
+        if (equation.includes ('+')) {
+            let numArray = equation.split('+');
+            mathDoer(numArray[0], '+', numArray[1]);
+        }
+        if (equation.includes ('-')) {
+            let numArray = equation.split('-');
+            mathDoer(numArray[0], '-', numArray[1]);
+        }
+        if (equation.includes ('/')) {
+            let numArray = equation.split('/');
+            mathDoer(numArray[0], '/', numArray[1]);
+        }
+        if (equation.includes ('*')) {
+            let numArray = equation.split('*');
+            mathDoer(numArray[0], '*', numArray[1]);
+        }
     }
 }
+
+function mathDoer (num1, operator, num2) {
+
+
+}
+
 
 module.exports = equationHandler;
